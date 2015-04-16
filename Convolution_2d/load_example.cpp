bb8865765212a976edaa9dadb6d3753b27eaed8f
daniel <daniel@sirius.utp.edu.co>
2015-04-16 18:14:00 -0500

    Add Secuential filter

--- Convolution_2d/load_example.cpp
+++ Convolution_2d/load_example.cpp
@@ -5,12 +5,27 @@
 using namespace cv;
 using namespace std;
 
-int main(){		
+int main(){
+  	
+  	int scale = 1;
+  	int delta = 0;
+  	int ddepth = CV_8UC1;
   
     Mat image;
     image = imread("inputs/img1.jpg",0);   // Read the file
-  	imwrite("./outputs/1088015148.png",image);
-   
+  
+    /// Generate grad_x and grad_y
+    Mat grad_x, grad_y;
+
+    /// Gradient X                  
+    //   ( src  , grad_x, ddepth,dx,dy,scale,delta, BORDER_DEFAULT );
+    Sobel( image, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+    
+    /// Gradient Y
+    //Sobel( image, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+    
+		imwrite("./outputs/1088015148.png",grad_x);
 
     return 0;
 }
+
