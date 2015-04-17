30afbc2ae095a6301a7bb1455c7eba821e452b68
daniel <daniel@sirius.utp.edu.co>
2015-04-17 17:01:28 -0500

    Add a functional Serial Kernel

---
+++ Convolution_2d/Serial_opencv_kernel.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <cuda.h>
+#include <highgui.h>
+#include <cv.h>
+
+#define Mask_size  3
+using namespace cv;
+using namespace std;
+
+
+void filter(unsigned char * In,unsigned char *Out,char *Kernel,int Mask_width,int Rowimg,int Colimg){
+
+  for(int i=0 ; i< Rowimg *Colimg ; i++){
+    int Gap=i-(Mask_width)/2; // asymmetric Gap (Left Right)
+    int Value=0;
+      for(int j=0 ;j<Mask_width;j++){
+        if(Gap+j >= 0 && j+Gap<(Rowimg *Colimg)){
+          Value+=In[Gap+j]*Kernel[j];
+        }// end if
+      }
+      Out[i]=Value;
+  }
+
+}
+
+
+int main(){
+
+  Mat image,image_final;
+  image=imread("inputs/img1.jpg",0);
+  Size s = image.size();
+  int Row = s.width;
+  int Col = s.height;
+  int channels = image.channels();
+  unsigned char *image_data = (unsigned char *)malloc(sizeof(unsigned char)*Row*Col*channels);
+  unsigned char *image_out = (unsigned char *)malloc(sizeof(unsigned char)*Row*Col*channels);
+  char Kernel[] = {-1,-1,-1,0,0,0,1,1,1};
+  image_data=image.data;
+  filter(image_data,image_out,Kernel,Mask_size,Row,Col);
+  image_final.create(Row,Col,CV_8UC1);
+  image_final.data = image_out;
+  imwrite("./outputs/1088015148.png",image_final);
+
+  return 0;
+}
